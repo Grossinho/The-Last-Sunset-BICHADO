1237a6e207e0907442f0d86de9bbfbce4b30d4ef
Gabriel Duarte <gabrielduartemartinelli@hotmail.com>
2019-09-25 17:16:21 -0300

    Inicio da tela de Game Over

---
+++ The Last Sunset/The Last Sunset - UNITY/The Last Sunset - UNITY/Assets/Script/GameOverPal.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameOverPal : MonoBehaviour
+{
+
+    private void Start()
+    {
+        Time.timeScale = 1;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Time.timeScale = 0;
+            SceneManager.LoadScene("Game Over");
+        }
+   
+           
+    }
+
+}
