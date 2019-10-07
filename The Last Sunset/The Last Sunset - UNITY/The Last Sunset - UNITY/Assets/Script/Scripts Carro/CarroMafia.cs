bb4c6d4f0ebf6adeedb29e65f19b0c58b9176138
Felphz <felipejoliveira2@gmail.com>
2019-10-07 17:06:21 -0300

    Cronstruindo script do carro da mafia.

---
+++ The Last Sunset/The Last Sunset - UNITY/The Last Sunset - UNITY/Assets/Script/Scripts Carro/CarroMafia.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarroMafia : MonoBehaviour
+{
+
+
+
+    public GameObject player;
+
+    float offset;
+    float trans;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        trans = transform.position.x;
+        offset = transform.position.z - player.transform.position.z;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x, transform.position.y, offset + player.transform.position.z);
+
+
+
+        trans += trans + Mathf.Sin(Time.deltaTime) * 8;
+
+
+    }
+}
