3a9b35294c9ac8ea1b2c17ef31fb4623d7d6319a
matsalen <k.kurosaki@hotmail.com>
2019-11-06 17:22:43 -0300

    Recorde fino

--- The Last Sunset/The Last Sunset - UNITY/The Last Sunset - UNITY/Assets/Script/Recorde.cs
+++ The Last Sunset/The Last Sunset - UNITY/The Last Sunset - UNITY/Assets/Script/Recorde.cs
@@ -17,7 +17,6 @@ public class Recorde : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
         InvokeRepeating("AtualizaRecorde", 1, 1);
     }
 
@@ -45,13 +44,13 @@ public class Recorde : MonoBehaviour
             {
                 if (RecordAtual > recordes[1])
                 {
-                    CarregaRecorde(3, PlayerPrefs.GetFloat("Record2"), "Nome2");
-                    CarregaRecorde(2, PlayerPrefs.GetFloat("Record1"), "Nome1");
+                    CarregaRecorde(3, PlayerPrefs.GetFloat("Record2"), nomes[2]);
+                    CarregaRecorde(2, PlayerPrefs.GetFloat("Record1"), nomes[1]);
                     CarregaRecorde(1, RecordAtual, nomeAtual);
                 }
                 else
                 {
-                    CarregaRecorde(3, PlayerPrefs.GetFloat("Record2"), "Nome2");
+                    CarregaRecorde(3, PlayerPrefs.GetFloat("Record2"), nomes[2]);
                     CarregaRecorde(2, RecordAtual, nomeAtual);
                 }
 
@@ -59,10 +58,13 @@ public class Recorde : MonoBehaviour
             else CarregaRecorde(3, RecordAtual, nomeAtual);
         }
         PlayerPrefs.SetFloat("Record", 0);
+        PlayerPrefs.SetString("Nome", "AAA");
+
         textoRecorde.text = null;
         for (int i = 1; i < 4; i++)
         {
             textoRecorde.text += nomes[i] + ":" + " " + recordes[i].ToString() + Environment.NewLine;
+            Debug.Log(nomes[i]);
         }
     }
 }
